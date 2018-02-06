8f38b4e81195ea84658e79f6bf10b875856c564e
WangBinyu <fsm114000@163.com>
2018-02-07 01:23:39 +0800

    ..

--- client/src/components/JSONDisplay/JSONDisplay.vue
+++ client/src/components/JSONDisplay/JSONDisplay.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="json-display-container" v-show="displayChild">
-    <div class="style-row" :class="valueType !== 'object' ? 'handle-hover' : ''">
-      {{ valueType }} {{ valueType === 'object' }}
-      <ul v-if="valueType === 'object'">
-        <li>1</li>
+    <span v-if="k" class="style-key" @click="lap" :class="{object: 'style-object-key-start', array: 'style-array-key-start'}[valueType]">{{ k }}</span>
+    <span v-show="display">
+      <ul v-if="valueType === 'object'" class="json-ul">
         <li v-for="a in result" :key="a.name">
-          
-          <div class="style-row" :class="(a.type !== 'object' || a.value === null) ? 'handle-hover' : ''">
-            <span :class="a.type === 'object' ? 'style-warp-key' : 'style-key'">{{ a.key }}</span> :
-            <ul v-if="Array.isArray(a.value)">
+          <div class="style-row">
+            <!-- <span :class="a.type === 'object' ? 'style-warp-key' : 'style-key'">{{ a.key }}</span> : -->
+            <json :str="JSON.stringify(a.value)" :k="a.key"></json>
+          </div>
+        </li>
+      </ul>
+      <ul v-if="valueType === 'array'" class="json-ul">
+        <!-- <span class="style-warp-key">{{ a.key }}</span> : -->
+        <li v-for="a in result" :key="a.name">
+          <json :str="JSON.stringify(a)"></json>
+        </li>
+      </ul>
+      <span v-if="valueType === 'string'" class="style-str">{{ result }}</span>
+      <span v-if="valueType === 'number'" class="style-num">{{ result }}</span>
+    </span>  
+
+    <span v-if="k" :class="{object: 'style-object-key-end', array: 'style-array-key-end'}[valueType]"></span>
+      <!-- <ul v-if="Array.isArray(a.value)">
               <li v-for="b in a.value" :key="JSON.stringify(b)">
                 <json v-if="typeof b === 'object' && !Array.isArray(b) && a.value" :str="JSON.stringify(a.value)"></json>
               </li>
@@ -16,11 +29,7 @@
             <json v-if="a.type === 'object' && !Array.isArray(a.value) && a.value" :str="JSON.stringify(a.value)"></json>
             <span v-if="a.value === null" class="style-warp-key">null</span>
             <span v-if="a.value === undefined" class="style-undefined">undefined</span>
-            <span v-if="a.type !== 'object'" :class="{'number': 'style-num', 'string': 'style-str'}[a.type]">{{ a.value }}</span>
-          </div>
-        </li>
-      </ul>
-      <!-- <span v-if="Array.isArray(a.value)" :class="{'number': 'style-num', 'string': 'style-str'}[a.type]">{{ a.value }}</span> -->
+      <span v-if="Array.isArray(a.value)" :class="{'number': 'style-num', 'string': 'style-str'}[a.type]">{{ a.value }}</span> -->
       <!-- <ul v-if="Array.isArray(a.value)">
         <li v-for="b in a.value" :key="JSON.stringify(b)">
           <json v-if="typeof b === 'object' && !Array.isArray(b) && a.value" :str="JSON.stringify(a.value)"></json>
@@ -30,7 +39,6 @@
       <span v-if="type === 'null'" class="style-warp-key">null</span>
       <span v-if="type === 'undefined'" class="style-undefined">undefined</span>
       <span v-if="type === 'number' || type === 'string'" :class="{'number': 'style-num', 'string': 'style-str'}[a.type]">{{ a.value }}</span> -->
-    </div>
     <!-- <ul>
       <li v-for="a in list" :key="a.name">
         <div class="style-row" :class="(a.type !== 'object' || a.value === null) ? 'handle-hover' : ''">
@@ -53,45 +61,65 @@
 // 递归组件需要声明name
 export default {
   name: 'json',
-  props: ['str', 'show'],
+  props: ['str', 'k', 'show', 'last'],
   data () {
     return {
       displayChild: true,
-      _temp: ''
+      result: '',
+      display: true
+    }
+  },
+  methods: {
+    lap () {
+      if (this.valueType !== 'object' && this.valueType !== 'array') return
+
+      this.display = !this.display
     }
   },
   computed: {
-    result () {
-      let result
-      try {
-        console.log(1)
-        let result
-        const temp = JSON.parse(typeof this.str === 'string' ? this.str : JSON.stringify(this.str))
-        console.log(2, this.valueType)
-        if (this.valueType === 'object') {
-          result = []
-          for (let key in temp) {
-            result.push({ key, value: temp[key], type: typeof temp[key] })
-          }
-        } else {
-          result = temp
-        }
-        console.log(3)
-        console.log(result)
-      } catch (e) {} finally {}
+    // result () {
+    //   let result
+    //   try {
+    //     console.log(1)
+    //     let result
+    //     const temp = JSON.parse(typeof this.str === 'string' ? this.str : JSON.stringify(this.str))
+    //     console.log(2, this.valueType)
+    //     if (this.valueType === 'object') {
+    //       result = []
+    //       for (let key in temp) {
+    //         result.push({ key, value: temp[key], type: typeof temp[key] })
+    //       }
+    //     } else {
+    //       result = temp
+    //     }
+    //     console.log(3)
+    //     console.log(result)
+    //   } catch (e) {} finally {}
 
-      // result.sort((a, b) => a.key > b.key)
-      return result
-    },
+    //   // result.sort((a, b) => a.key > b.key)
+    //   return result
+    // },
     valueType () {
-      const temp = JSON.parse(typeof this.str === 'string' ? this.str : JSON.stringify(this.str))
-      console.log(typeof temp)
+      const temp = JSON.parse(this.str)
+      console.log(this.str)
       let type = typeof temp
       if (Array.isArray(temp)) type = 'array'
       else if (temp === null) type = 'null'
       else if (type === 'object') type = 'object'
       else if (temp === undefined) type = 'undefined'
-      this._temp = temp
+
+      let result
+      if (type === 'object') {
+        result = []
+        result = temp
+        // for (let key in temp) {
+        //   result.push({ key, value: temp[key], type: typeof temp[key] })
+        // }
+      } else {
+        result = temp
+      }
+
+      this.result = result
       return type
     }
   }
@@ -99,21 +127,48 @@ export default {
 </script>
 <style>
 .json-display-container {
-  font-family: 'Consola'
+  font-family: 'Consola'; display: inline-block; padding-left: 1em;
+}
+.json-ul {
+  padding-left: 0;
 }
 .style-key {
   color: #06c5e5; font-style: italic;
 }
 
-.style-key:hover { transform: scale(1.1); }
-.style-warp-key {
-  color: #fc1c71; font-weight: 900;
+.style-key::after {
+  content: " : "; font-style: normal;
 }
+
+.style-key:hover { transform: scale(1.1); }
+
+.style-object-key-start {
+  color: #fc1c71; font-weight: 900; cursor: pointer;
+}
+.style-object-key-start::after {
+  content: " : {"; color: #fc1c71; font-weight: normal;
+}
+
+.style-object-key-end::after {
+  content: "}"; color: #fc1c71; font-style: normal;
+}
+
+.style-array-key-start {
+  color: #fc1c71; font-weight: 900; cursor: pointer;
+}
+.style-array-key-start::after {
+  content: " : ["; color: #fc1c71; font-style: normal; font-weight: normal;
+}
+
+.style-array-key-end::after {
+  content: "]"; color: #fc1c71; font-style: normal;
+}
+
 .style-warp-key:hover {
   cursor: pointer;
 }
 .style-warp-key::after {
-  content: ":", count
+  content: ":"
 }
 
 .style-num {
